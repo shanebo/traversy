f2b7563fcaf7fa1d18566c00e7c201db63972399
Shane Thacker <shane@steadymade.com>
2018-01-22 15:13:30 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Traversy
+===================
+
+Dead simple directory traversal with regex filtering and callbacks on matches.
+
+Released under a [MIT license](http://en.wikipedia.org/wiki/MIT_License).
+
+Install
+--------
+
+`npm install traversy`
+
+Usage
+-----
+
+``` js
+const traversy = require('traversy');
+traversy('path/to/directory');
+// returns array of paths
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+function dirToArray(path, filter) {
+  if (fs.statSync(path).isDirectory()) {
+    return fs.readdirSync(path)
+      .map(item => dirToArray(`${path}/${item}`, filter))
+      .reduce((total, current) => total.concat(current), []);
+  } else {
+    return [path].filter(path => new RegExp(filter, 'g').test(path));
+  }
+}
+
+module.exports = (dir, filter, cb) => {
+  return dirToArray(dir, filter).forEach(path => cb(path));
+}
